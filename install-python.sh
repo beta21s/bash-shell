846bd21d35d0de9d1529c794c519176f87ff2d38
Trần Phan An Trường <66484965+beta21s@users.noreply.github.com>
2022-09-21 20:01:25 +0700

    Create install-python.sh

---
+++ install-python.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.8
+apt install python3-pip
+python3 --version
